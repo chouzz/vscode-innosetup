723c1864dfc9aa38999a0943a966a0061d55e4e0
Jan T. Sott <jan@idleberg.com>
2017-09-16 23:48:52 +0200

    first commit

---
+++ src/build.ts
@@ -0,0 +1,46 @@
+'use strict';
+
+import { platform } from 'os';
+import { spawn } from 'child_process';
+import { window } from 'vscode';
+
+const nsisChannel = window.createOutputChannel('Inno Setup');
+
+const build = () => {
+  const config = vscode.workspace.getConfiguration('innosetup');
+
+  if (config.pathToIscc === 'ISCC.exe' && platform() !== 'win32') {
+    return window.showWarningMessage('This command is only available on Windows. See README for workarounds on non-Windows.');
+  }
+
+  const doc = window.textEditor.document;
+
+  doc.save().then( () => {
+    outputChannel.clear();
+    if (config.alwaysShowOutput === true) {
+      outputChannel.show();
+    }
+
+    // Let's build
+    const iscc = spawn(config.pathToIscc, [ doc.fileName ]);
+
+    iscc.stdout.on('data', (data) => {
+      outputChannel.appendLine(data.toString());
+    });
+
+    iscc.stderr.on('data', (data) => {
+      outputChannel.appendLine(data.toString());
+    });
+
+    iscc.on('close', (code) => {
+      if (code === 0) {
+        if (config.showNotifications) window.showInformationMessage(`Successfully compiled '${doc.fileName}'`);
+      } else {
+        outputChannel.show(true);
+        if (config.showNotifications) window.showErrorMessage('Compilation failed, see output for details');
+      }
+    });
+  });
+};
+
+export { build };
---
+++ src/task.ts
@@ -0,0 +1,55 @@
+'use babel';
+
+import { workspace, window } from 'vscode';
+
+import { mkdir, writeFile } from 'fs';
+import { join } from 'path';
+
+const { version } = require('../package.json');
+
+const task = () => {
+  if (typeof workspace.rootPath === 'undefined' || workspace.rootPath === null) {
+    return window.showErrorMessage('Task support is only available when working on a workspace folder. It is not available when editing single files.');
+  }
+
+  const config = workspace.getConfiguration('innosetup');
+  const command: string = config.pathToIscc;
+
+  const taskFile: Object = {
+      'command': command,
+      'version': version,
+      'args': [],
+      'isShellCommand': false,
+      'showOutput': 'always',
+      'suppressTaskName': true,
+      'echoCommand': false,
+      'tasks': [
+        {
+          'taskName': 'Build',
+          'args': [ '${file}' ],
+          'isBuildCommand': true
+        }
+      ]
+  };
+
+  const jsonString: string = JSON.stringify(taskFile, null, 2);
+  const dotFolder: string = join(workspace.rootPath, '/.vscode');
+  const buildFile: string = join(dotFolder, 'tasks.json');
+
+  mkdir(dotFolder, (error) => {
+    // ignore errors for now
+    writeFile(buildFile, jsonString, (error) => {
+      if (error) {
+        return window.showErrorMessage(error);
+      }
+      if (config.alwaysOpenBuildTask === false) return;
+
+      // Open tasks.json
+      workspace.openTextDocument(buildFile).then( (doc) => {
+          window.showTextDocument(doc);
+      });
+    });
+  });
+};
+
+export { task };
