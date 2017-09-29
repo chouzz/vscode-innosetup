c29bd789136abf7a2e679cd2653845475c429f83
Jan T. Sott <jan@idleberg.com>
2017-09-29 21:35:22 +0200

    add Run button

--- src/build.ts
+++ src/build.ts
@@ -4,11 +4,12 @@ import { workspace, window } from 'vscode';
 
 import { platform } from 'os';
 import { spawn } from 'child_process';
+import { detectOutfile, getConfig, runInstaller } from './util';
 
 const outputChannel = window.createOutputChannel('Inno Setup');
 
 const build = () => {
-  const config = workspace.getConfiguration('innosetup');
+  const config: any = getConfig();
 
   if (config.pathToIscc === 'ISCC.exe' && platform() !== 'win32') {
     return window.showWarningMessage('This command is only available on Windows. See README for workarounds on non-Windows.');
@@ -25,17 +26,31 @@ const build = () => {
     // Let's build
     const iscc = spawn(config.pathToIscc, [ doc.fileName ]);
 
-    iscc.stdout.on('data', (data) => {
-      outputChannel.appendLine(data.toString());
+    let outFile: string = '';
+
+    iscc.stdout.on('data', (line: Array<any>) => {
+      outputChannel.appendLine(line.toString());
+
+      if (platform() === 'win32' && outFile === '') {
+        outFile = detectOutfile(line);
+      }
     });
 
-    iscc.stderr.on('data', (data) => {
-      outputChannel.appendLine(data.toString());
+    iscc.stderr.on('data', (line:  Array<any>) => {
+      outputChannel.appendLine(line.toString());
     });
 
     iscc.on('close', (code) => {
+      let openButton = (platform() === 'win32' && outFile !== '') ? 'Run' : null;
       if (code === 0) {
-        if (config.showNotifications) window.showInformationMessage(`Successfully compiled '${doc.fileName}'`);
+        if (config.showNotifications) {
+          window.showInformationMessage(`Successfully compiled '${doc.fileName}'`, openButton)
+          .then((choice) => {
+            if (choice === 'Run') {
+              runInstaller(outFile);
+            }
+          });
+        }
       } else {
         outputChannel.show(true);
         if (config.showNotifications) window.showErrorMessage('Compilation failed, see output for details');
