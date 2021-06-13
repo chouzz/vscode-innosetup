2278b5e9287d6b92332547805cd4b87074686e64
idleberg <git@idleberg.com>
2021-06-13 02:38:56 +0200

    first commit

---
+++ src/channel.ts
@@ -0,0 +1,48 @@
+import vscode from 'vscode';
+import { getConfig } from 'vscode-get-config';
+
+export default {
+  outputChannel: vscode.window.createOutputChannel('Inno Setup'),
+
+  async clear(): Promise<void> {
+    this.outputChannel.clear();
+
+    const { alwaysShowOutput } = await getConfig('nsis');
+
+    if (alwaysShowOutput) {
+      this.show();
+    }
+  },
+
+  dispose(): void {
+    this.outputChannel.dispose();
+  },
+
+  async append(input: unknown): Promise<void> {
+    this.outputChannel.append(input);
+
+    const { alwaysShowOutput } = await getConfig('nsis');
+
+    if (alwaysShowOutput) {
+      this.show();
+    }
+  },
+
+  async appendLine(input: string): Promise<void> {
+    this.outputChannel.appendLine(input);
+
+    const { alwaysShowOutput } = await getConfig('nsis');
+
+    if (alwaysShowOutput) {
+      this.show();
+    }
+  },
+
+  hide(): void {
+    this.outputChannel.hide();
+  },
+
+  show(preserveFocus = true): void {
+    this.outputChannel.show(preserveFocus);
+  }
+}
