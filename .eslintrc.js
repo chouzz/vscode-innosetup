bd414502967e495f548f17529e594475caba0549
chouzz <zhouhua25@qq.com>
2022-08-01 00:10:04 +0800

    Update repo configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,25 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint",
-    "@typescript-eslint/eslint-plugin-tslint",
-    "eslint-plugin-import",
-  ],
-  env: {
-    node: true,
-  },
-  rules: {
-    "@typescript-eslint/no-var-requires": "error",
-    "@typescript-eslint/semi": "error",
-    "comma-dangle": "error",
-    curly: "error",
-    "eol-last": "error",
-    "import/no-default-export": "error",
-    "no-var": "error",
-    "prefer-const": "error",
-  },
-  ignorePatterns: ["*.d.ts"],
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+        '@typescript-eslint/eslint-plugin-tslint',
+        'eslint-plugin-import',
+    ],
+    env: {
+        node: true,
+    },
+    rules: {
+        '@typescript-eslint/no-var-requires': 'error',
+        '@typescript-eslint/semi': 'error',
+        'comma-dangle': 'error',
+        curly: 'error',
+        'eol-last': 'error',
+        'import/no-default-export': 'error',
+        'no-var': 'error',
+        'prefer-const': 'error',
+        quotes: 'off',
+        '@typescript-eslint/quotes': ['error', 'single'],
+    },
+    ignorePatterns: ['*.d.ts'],
 };
