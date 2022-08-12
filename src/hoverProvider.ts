90acccead5920f72bd1d836d3b66337604cc8056
chouzz <zhouhua25@qq.com>
2022-08-12 23:26:58 +0800

    Basic on hover feature implemention

--- src/hoverProvider.ts
+++ src/hoverProvider.ts
@@ -1,20 +1,44 @@
 import * as vscode from 'vscode';
+import { readFile } from 'fs/promises';
+import * as path from 'path';
+
+export async function activate(context: vscode.ExtensionContext) {
+    const constantMapPath = path.join(
+        context.extensionPath,
+        'script',
+        'constant.json',
+    );
+    const constantsItems = await readFile(constantMapPath, {
+        encoding: 'utf8',
+    });
+    const constItemsMap = new Map<string, string>(
+        Object.entries(JSON.parse(constantsItems)),
+    );
+    const hoverProvider = new InnosetupHoverProvder(constItemsMap);
+
+    context.subscriptions.push(
+        vscode.languages.registerHoverProvider(
+            { language: 'innosetup' },
+            hoverProvider,
+        ),
+    );
+}
 
 export class InnosetupHoverProvder implements vscode.HoverProvider {
-    constructor(private _wordMap: Map<string, string>) {}
+    constructor(private _constantItems: Map<string, string>) {}
     public provideHover(
         document: vscode.TextDocument,
         position: vscode.Position,
         token: vscode.CancellationToken,
     ): vscode.ProviderResult<vscode.Hover> {
-        const tokenRange = document.getWordRangeAtPosition(position);
+        const tokenRange = document.getWordRangeAtPosition(position, /\{\w+\}/g);
         const tokenText = document.getText(tokenRange);
-        if (this._wordMap.has(tokenText)) {
-            return new vscode.Hover({
-                language: 'xml',
-                value: this._wordMap.get(tokenText),
-            });
+        // Remove '{' and '}'
+        if (this._constantItems.has(tokenText.slice(1, -1))) {
+            const description = this._constantItems.get(tokenText.slice(1, -1));
+            const markdown = new vscode.MarkdownString(description);
+            markdown.supportHtml = true;
+            return new vscode.Hover(markdown);
         }
-        // FIXME : support constant like {app}
     }
 }
