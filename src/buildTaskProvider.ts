4b779ef91257454a0c5b44b6ea56f1a39a5732c2
Chouzz <zhouhua25@qq.com>
2023-04-13 23:58:08 +0800

    Rework taks provider and fix problem matcher if compile via tasks (#117)
    We can run compile via vscode tasks, but the problem matcher didn't
    work, this patch fix the problem.
    
    Co-authored-by: chouzz <zhouhua852@qq.com>

--- src/buildTaskProvider.ts
+++ src/buildTaskProvider.ts
@@ -1,10 +1,23 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as cp from 'child_process';
+
+export interface BuildTaskDefinition extends vscode.TaskDefinition {
+    type: string;
+    label: string;
+    command: string;
+    args: string[];
+    options: cp.ExecOptions | undefined;
+}
 
 export class BuildTaskProvider implements vscode.TaskProvider {
-    public provideTasks(
-        token?: vscode.CancellationToken,
-    ): vscode.ProviderResult<vscode.Task[]> {
+    static buildScriptType = 'innosetup-build';
+    static buildSourceStr = 'innosetup';
+    private tasks: vscode.Task[] | undefined;
+    public provideTasks(token?: vscode.CancellationToken): vscode.ProviderResult<vscode.Task[]> {
+        if (this.tasks) {
+            return this.tasks;
+        }
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
             return [];
@@ -14,35 +27,42 @@ export class BuildTaskProvider implements vscode.TaskProvider {
         if (!isIssFile) {
             return [];
         }
-        const excutable =
-            vscode.workspace
-                .getConfiguration('innosetup')
-                .get<string>('pathToIscc') || '';
-        const execution = new vscode.ShellExecution(excutable, [
-            {
-                value: file,
-                quoting: vscode.ShellQuoting.Strong,
-            },
-        ]);
-        const taskDef = {
-            type: 'innosetup',
-        };
-        const task = new vscode.Task(
-            taskDef,
-            vscode.TaskScope.Workspace,
-            'compile script',
-            'innosetup',
-            execution,
-            ['innosetup-6-error', 'innosetup-5-error', 'innosetup-5-warning'],
-        );
-        task.source = 'innosetup';
-        task.group = vscode.TaskGroup.Build;
-        return [task];
+        const compilerPath = vscode.workspace.getConfiguration('innosetup').get<string>('pathToIscc') || '';
+        this.tasks = [this.getTask(compilerPath)];
+        return this.tasks;
     }
-    public resolveTask(
-        task: vscode.Task,
-        token?: vscode.CancellationToken,
-    ): vscode.ProviderResult<vscode.Task> {
+    public resolveTask(task: vscode.Task, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.Task> {
+        return this.getTask(task.definition.command, task.definition.args, task.definition as BuildTaskDefinition);
+    }
+
+    private getTask(compilerPath: string, compilerArgs?: string[], definition?: BuildTaskDefinition) {
+        const taskLabel = 'compile script';
+        const cwd = '${workspaceFolder}';
+        const args = compilerArgs && compilerArgs.length ? compilerArgs: ['${file}'];
+        const options: cp.ExecOptions | undefined = { cwd: cwd };
+        if (!definition) {
+            definition = {
+                type: BuildTaskProvider.buildScriptType,
+                label: taskLabel,
+                command: compilerPath,
+                args: args,
+                options: options,
+            };
+        }
+        const scope = vscode.TaskScope.Workspace;
+        const task = new vscode.Task(
+            definition,
+            scope,
+            taskLabel,
+            BuildTaskProvider.buildSourceStr,
+            new vscode.ShellExecution(compilerPath, args, {
+                cwd: cwd
+            }),
+            ['$innosetup-6-error', '$innosetup-5-error', '$innosetup-5-warning'],
+        );
+        task.group = vscode.TaskGroup.Build;
+        task.detail = 'compiler: ' + compilerPath;
+
         return task;
     }
 }
